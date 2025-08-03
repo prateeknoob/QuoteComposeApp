a256ac654c04b120338f20e3d4ae633eb67d57f9
Prateek345 <prateeksingh.ps345@gmail.com>
2025-08-03 23:09:06 +0530

    changes

--- app/src/main/java/com/example/quotecomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/quotecomposeapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.quotecomposeapp.screens.QuoteDetail
 import com.example.quotecomposeapp.ui.theme.QuoteComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            QuoteComposeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            QuoteDetail()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuoteComposeAppTheme {
-        Greeting("Android")
     }
 }
