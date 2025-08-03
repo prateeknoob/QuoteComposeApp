ac8153c673ebcebeeeac0ca57318b7121457e1d3
Prateek345 <prateeksingh.ps345@gmail.com>
2025-08-03 23:13:12 +0530

    changes

--- app/src/main/java/com/example/quotecomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/quotecomposeapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.quotecomposeapp
 
+import QuoteDetail
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.quotecomposeapp.screens.QuoteDetail
 import com.example.quotecomposeapp.ui.theme.QuoteComposeAppTheme
 
 class MainActivity : ComponentActivity() {
